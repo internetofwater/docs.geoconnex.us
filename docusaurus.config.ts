94c6f27957b813ee069da5589fa531dea26de466
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-07-14 15:22:26 -0400

    don't err on broken links for time being until website is in better state

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,8 +18,8 @@ const config: Config = {
   organizationName: "internetofwater", // Usually your GitHub org/user name.
   projectName: "docs.geoconnex.us", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "throw",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
