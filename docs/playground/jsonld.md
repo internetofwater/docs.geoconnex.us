510a94379800e2c3116113bde10aac265c96957e
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-10-03 16:53:42 -0400

    off by one

--- docs/playground/jsonld.md
+++ docs/playground/jsonld.md
@@ -4,3 +4,18 @@ hide_title: true
 ---
 
 <iframe width="100%" height="1300px" src="https://tinyurl.com/29qaectm" title="JSON-LD playground"></iframe>
+
+```json
+// !collapse(1:2)
+sfdsds
+dfs
+dsf
+above
+// !collapse(1:3)
+below
+bob
+joe
+sdfd
+sf
+```
+
--- src/components/CodeHike/collapse.tsx
+++ src/components/CodeHike/collapse.tsx
@@ -1,90 +1,80 @@
+import { AnnotationHandler, BlockAnnotation, InnerLine } from "codehike/code";
+import { ArrowRightCircle, ArrowDownCircle } from "lucide-react";
+import React, { useState } from "react"; // Import useState
 import {
-    AnnotationHandler,
-    BlockAnnotation,
-    InnerLine
-  } from "codehike/code";
-  import {
-    ArrowRightCircle,
-  } from "lucide-react";
-  import React from "react";
-  import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger
-  } from '@radix-ui/react-collapsible';
-  
-  export const collapse: AnnotationHandler = {
-    name: "collapse",
-    transform: (annotation: BlockAnnotation) => {
-      const {
-        fromLineNumber
-      } = annotation;
-      return [
-        annotation,
-        {
-          ...annotation,
-          fromLineNumber: fromLineNumber,
-          toLineNumber: fromLineNumber,
-          name: "CollapseTrigger",
-        },
-        {
-          ...annotation,
-          fromLineNumber: fromLineNumber + 1,
-          name: "CollapseContent",
-        },
-      ];
-    },
-    Block: ({
-      annotation,
-      children
-    }) => {
-      return (
-        <Collapsible defaultOpen={annotation.query !== "collapsed"}>
-          {children}
-        </Collapsible>
-      );
-    },
-  };
-  
-  const icon = (
-    <ArrowRightCircle
-      style={{
-        display: 'inline-block',
-        marginTop: '1px',
-        transition: 'transform 0.2s, opacity 0.2s',
-        opacity: '0.8',
-        marginBottom: '1px',
-      }}
-      size={15}
-    />
-  );
-  
-  export const collapseTrigger: AnnotationHandler = {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@radix-ui/react-collapsible";
 
-    name: "CollapseTrigger",
-    onlyIfAnnotated: true,
-    AnnotatedLine: ({ annotation, ...props }) => {
-      return (
-        <CollapsibleTrigger>
-          <InnerLine merge={props} data={{ icon }} />
+export const collapse: AnnotationHandler = {
+  name: "collapse",
+  transform: (annotation: BlockAnnotation) => {
+    const { fromLineNumber } = annotation;
+    return [
+      annotation,
+      {
+        ...annotation,
+        fromLineNumber: fromLineNumber, // Keep the fromLineNumber the same
+        toLineNumber: fromLineNumber, // Keep the toLineNumber the same
+        name: "CollapseTrigger",
+      },
+      {
+        ...annotation,
+        fromLineNumber: fromLineNumber + 1, // Keep this as is to set the content line
+        name: "CollapseContent",
+      },
+    ];
+  },
+  Block: ({ annotation, children }) => {
+    return (
+      <Collapsible defaultOpen={annotation.query === "collapsed"}>
+        {children}
+      </Collapsible>
+    );
+  },
+};
+
+export const collapseTrigger: AnnotationHandler = {
+  name: "CollapseTrigger",
+  onlyIfAnnotated: true,
+  AnnotatedLine: ({ annotation, ...props }) => {
+    const [isOpen, setIsOpen] = useState(false); // Local state to manage open/close
+
+    return (
+      <>
+        <InnerLine merge={props} />
+        <CollapsibleTrigger
+          style={{
+            backgroundColor: "transparent",
+            padding: 0,
+            border: "none",
+            position: "absolute",
+            left: 0,
+          }}
+          onClick={() => setIsOpen((prev) => !prev)} // Toggle the open state on click
+        >
+          {isOpen ? (
+            <ArrowDownCircle size={14} cursor={"pointer"} /> // Down arrow when open
+          ) : (
+            <ArrowRightCircle size={14} cursor={"pointer"} /> // Side arrow when closed
+          )}
         </CollapsibleTrigger>
-      );
-    },
-    Line: (props) => {
-      const icon = props.data?.icon as React.ReactNode;
-      return (
-        <div style={{ display: 'table-row' }}>
-            {icon}
-          <div style={{ display: 'table-cell' }}>
-            <InnerLine merge={props} />
-          </div>
-        </div>
-      );
-    },
-  };
-  
-  export const collapseContent: AnnotationHandler = {
-    name: "CollapseContent",
-    Block: CollapsibleContent,
-  };
-  
+      </>
+    );
+  },
+  Line: (props) => {
+    const icon = props.data?.icon as React.ReactNode;
+    return (
+      <>
+        <div style={{ display: "table-row" }}>{icon}</div>
+        <InnerLine merge={props} />
+      </>
+    );
+  },
+};
+
+export const collapseContent: AnnotationHandler = {
+  name: "CollapseContent",
+  Block: CollapsibleContent,
+};
